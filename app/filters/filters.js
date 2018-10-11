217f5582233c375009a94c63f61d58330cd7996f
Dayane <dayane92ferreira@gmail.com>
2018-10-11 00:26:33 -0300

    após inserção de filtros

--- app/filters/filters.js
+++ app/filters/filters.js
@@ -2,6 +2,7 @@
   "use strict";
   angular.module('bomControle')
 
+
     .filter('cpf', [function () {
       return function (text) {
         return text ? String(text).replace(/^(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4") : '';
